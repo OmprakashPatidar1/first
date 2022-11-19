f5c0df3eb57e76db0be38b0a41a85be0e3236105
OmprakashPatidar1 <patidaromprakash051@gmail.com>
2022-11-19 21:47:44 +0530

    #

---
+++ demo.js
@@ -0,0 +1,7 @@
+var header = document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+
+
+var items=document.getElementById('items');
+   items.style.fontWeight='bold';
+   items.style.color='green'
