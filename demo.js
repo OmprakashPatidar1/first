7738be7b980a882a86370496834cd51ee13fafab
OmprakashPatidar1 <patidaromprakash051@gmail.com>
2022-11-20 11:09:13 +0530

    #

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
 var header = document.getElementById('main-header');
 header.style.borderBottom='solid 3px #000';
 
+
 var header = document.getElementById('title');
 header.style.fontWeight='bold';
 header.style.color='green'
@@ -9,3 +10,12 @@ var items=document.getElementsByClassName('list-group-item');
   for(let i=0;i<items.length;i++){
       items[i].style.fontWeight='bold'
     }
+
+
+    
+    
+    var lastitem=document.getElementsByClassName('lis')
+    lastitem[0].style.fontWeight='bold'
+    
+    var l=document.getElementsByTagName('li');
+    l[4].style.backgroundColor='green'
