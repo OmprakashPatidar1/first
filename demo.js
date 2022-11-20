c60c6e0a0f3014b70ac90d9ba6e09f3536fe604b
OmprakashPatidar1 <patidaromprakash051@gmail.com>
2022-11-20 16:55:57 +0530

    querySelector

--- demo.js
+++ demo.js
@@ -1,21 +1,28 @@
-var header = document.getElementById('main-header');
-header.style.borderBottom='solid 3px #000';
+// var header = document.getElementById('main-header');
+// header.style.borderBottom='solid 3px #000';
 
 
-var header = document.getElementById('title');
-header.style.fontWeight='bold';
-header.style.color='green'
-var items=document.getElementsByClassName('list-group-item');
-   items[2].style.backgroundColor='green'
-  for(let i=0;i<items.length;i++){
-      items[i].style.fontWeight='bold'
-    }
+// var header = document.getElementById('title');
+// header.style.fontWeight='bold';
+// header.style.color='green'
+// var items=document.getElementsByClassName('list-group-item');
+//    items[2].style.backgroundColor='green'
+//   for(let i=0;i<items.length;i++){
+//       items[i].style.fontWeight='bold'
+//     }
 
 
     
     
-    var lastitem=document.getElementsByClassName('lis')
-    lastitem[0].style.fontWeight='bold'
+//     var lastitem=document.getElementsByClassName('lis')
+//     lastitem[0].style.fontWeight='bold'
     
-    var l=document.getElementsByTagName('li');
-    l[4].style.backgroundColor='green'
+//     var l=document.getElementsByTagName('li');
+//     l[4].style.backgroundColor='green'
+
+    var seconditem=document.querySelector('.list-group-item:nth-child(2)')
+    seconditem.style.backgroundColor='green'
+    var thirditem=document.querySelector('.list-group-item:nth-child(3)');
+    thirditem.style.display='none';
+
+
