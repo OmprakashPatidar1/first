123c6756e23ef00176b743b3b63e14595264c29e
OmprakashPatidar1 <patidaromprakash051@gmail.com>
2022-11-21 21:04:37 +0530

    filter

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 var form=document.getElementById('addForm');
-var itemList=document.getElementById('items');
-
+var itemList=document.getElementById('items');   
+var filter=document.getElementById('filter')
 //form submit event
 form.addEventListener('submit',addItem);
 
@@ -9,13 +9,16 @@ form.addEventListener('submit',addItem);
 //delete event
 itemList.addEventListener('click',removeItem)
 
+//filter event
+filter.addEventListener('keyup',filterItems)
 // add item
 function addItem(e){
     e.preventDefault();
-   
+    
     //get input value
     var newItem= document.getElementById('item').value;
-
+    var newItem1= document.getElementById('item1').value;
+    
     // create new li element
     var li = document.createElement('li');
     //add class
@@ -24,6 +27,7 @@ function addItem(e){
     //add text node  with input value
     li.appendChild(document.createTextNode(newItem))
     
+    li.appendChild(document.createTextNode(newItem1))
     //create del button element
     var deleteBtn=document.createElement('button');
     
@@ -35,19 +39,39 @@ function addItem(e){
     
     //append button to li
     li.appendChild(deleteBtn);
-
+    
 
     //append li to list
     itemList.appendChild(li);
 }
 
+
 // remove item
 function removeItem(e){
     if(e.target.classList.contains('delete')){
-     if(confirm('Are You Sure?')){
-        var li=e.target.parentElement;
-        itemList.removeChild(li);
-     }
+        if(confirm('Are You Sure?')){
+            var li=e.target.parentElement;
+            itemList.removeChild(li);
+        }
     }
 }
 
+
+// filter item
+function filterItems(e){
+    //convert text to lowercase
+    var text=e.target.value.toLowerCase();
+    //get list
+    var items=itemList.getElementsByTagName('li');
+
+    //convert to an array
+    Array.from(items).forEach(function(item) {
+        var itemName=item.firstChild.textContent;
+        var description=item.childNodes[1].textContent;
+        if(itemName.toLowerCase().indexOf(text)!=-1 || description.toLowerCase().indexOf(text)!=-1){
+            item.style.display='block';     
+        }else{
+            item.style.display='none';
+        }
+    })
+}
